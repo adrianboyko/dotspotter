42ba1bdc72a6b95478b62227e66a72df5cc26012
Adrian Boyko <adrianboyko@gmail.com>
2016-07-28 13:08:47 -0700

    Fallback position for WIP. Imgsum can now calculate grand row/col totals for all images. Grand totals can be used to determine background image to subtract from subsequent images. Previous use case (without grand totals and background) tested.

--- imgsum.c
+++ imgsum.c
@@ -1,7 +1,7 @@
 /*
  * imgsum.c
  * 
- * Copyright 2014 Adrian Boyko
+ * Copyright 2016 Adrian Boyko
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,25 +25,52 @@
 #include <stdio.h>
 
 typedef struct {
-	FILE* file;         // File where sums are written
-	uint32_t* rowSums;  // Row sum accumulators
-	uint32_t* colSums;  // Col sum accumulators
-	uint16_t width;     // Width of image, in pixels
-	uint16_t height;    // Height of image, in pixels
+	FILE* file;          // File where sums are written
+	uint32_t* rowSums;   // Row sum accumulators
+	uint32_t* colSums;   // Col sum accumulators
+	uint32_t* gtRowSums; // Grand total accumulator
+	uint32_t* gtColSums; // Grand total accumulator
+	uint32_t* rowBGs;    // Background to subtract from row sums
+	uint32_t* colBGs;    // Background to subtract from col sums
+	uint16_t  width;     // Width of image, in pixels
+	uint16_t  height;    // Height of image, in pixels
+	uint32_t  imgCount;  // How many imgs have been summed
 } modulestate;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// From http://stackoverflow.com/questions/3437404/min-and-max-in-c
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+     
+static void addArrayToArray_uint32(uint32_t* target, uint32_t* toAdd, size_t len) {
+	for (int i=0; i < len; i++) {
+		target[i] += toAdd[i]; // Overflow not expected.
+	}
+}
+
+static void subtractArrayFromArray_uint32(uint32_t* target, uint32_t* toSub, size_t len) {
+	for (int i=0; i < len; i++) {
+		uint32_t s = min(toSub[i], target[i]); // Prevent expected underflow.
+		target[i] -= s;
+	}
+}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 static PyObject* imgsum_start(PyObject* self, PyObject* args) {
 	modulestate* ms = PyModule_GetState(self);
 	
-	char* filename = NULL;
-	PyObject* wantRowSumsObj = NULL;
-	PyObject* wantColSumsObj = NULL;
+	ms->imgCount = 0;
 	
-	// TODO: Make range ends/begins default
-	if (!PyArg_ParseTuple(args, "iisOO", 
-	  &ms->width, &ms->height, &filename, &wantRowSumsObj, &wantColSumsObj)) {
+	PyObject* wantRowSumsObj     = NULL;
+	PyObject* wantColSumsObj     = NULL;
+	PyObject* wantGrandTotalsObj = NULL;
+	
+	if (!PyArg_ParseTuple(args, "iiOOO", 
+	  &ms->width, &ms->height, &wantRowSumsObj, &wantColSumsObj, &wantGrandTotalsObj)) {
 		return NULL;
 	}
 	
@@ -57,29 +84,40 @@ static PyObject* imgsum_start(PyObject* self, PyObject* args) {
 	
 	int wantRowSums = PyObject_IsTrue(wantRowSumsObj);
 	int wantColSums = PyObject_IsTrue(wantColSumsObj);
-	
-	ms->file = fopen(filename, "wb");
-	if (ms->file == NULL) {
-		PyErr_SetString(PyExc_EnvironmentError, "Couldn't open output file.");
-		return NULL;
-	}
+	int wantGrandTotals = PyObject_IsTrue(wantGrandTotalsObj);
 		
 	size_t rowSumsByteCount = ms->height * sizeof(*ms->rowSums);
 	size_t colSumsByteCount = ms->width * sizeof(*ms->colSums);
+	size_t gtRowSumsByteCount = ms->height * sizeof(*ms->gtRowSums);		
+	size_t gtColSumsByteCount = ms->width * sizeof(*ms->gtColSums);
+		
 	ms->rowSums = wantRowSums ? malloc(rowSumsByteCount) : NULL;
 	ms->colSums = wantColSums ? malloc(colSumsByteCount) : NULL;
+	ms->gtRowSums = wantRowSums && wantGrandTotals ? malloc(gtRowSumsByteCount) : NULL;
+	ms->gtColSums = wantColSums && wantGrandTotals ? malloc(gtColSumsByteCount) : NULL;
+	if (ms->gtRowSums) memset(ms->rowSums, 0, gtRowSumsByteCount);
+	if (ms->gtColSums) memset(ms->colSums, 0, gtColSumsByteCount);
 		
 	Py_RETURN_NONE;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-static PyObject* imgsum_stop(PyObject* self, PyObject* args) {
+static PyObject* imgsum_setFile(PyObject* self, PyObject* args) {
 	modulestate* ms = PyModule_GetState(self);
+
+	char* filename = NULL;
+
+	if (!PyArg_ParseTuple(args, "s", &filename)) {
+		return NULL;
+	}
+
+	ms->file = fopen(filename, "wb");
+	if (ms->file == NULL) {
+		PyErr_SetString(PyExc_EnvironmentError, "Couldn't open output file.");
+		return NULL;
+	}
 	
-	if (ms->rowSums) free(ms->rowSums);
-	if (ms->colSums) free(ms->colSums);
-	fclose(ms->file);
 	Py_RETURN_NONE;
 }
 
@@ -97,7 +135,7 @@ static PyObject* imgsum_sum(PyObject* self, PyObject* args) {
 		return NULL;
 	}
 		
-	// Calculate the row sums for the given image:
+	// Calculate the sums for the given image:
 	char* val = yuv420;
 	uint16_t currRow = 0;
 	uint16_t currCol = 0;
@@ -113,18 +151,87 @@ static PyObject* imgsum_sum(PyObject* self, PyObject* args) {
             currCol = 0;
         }
     }
-	if (ms->rowSums) fwrite(ms->rowSums, rowSumsByteCount, 1, ms->file);
-	if (ms->colSums) fwrite(ms->colSums, colSumsByteCount, 1, ms->file);
+    
+    // Subtract background, write sums to file, add sums to grand totals.
+	if (ms->rowSums) {
+		if (ms->rowBGs) subtractArrayFromArray_uint32(ms->gtRowSums, ms->rowBGs, ms->height);
+		if (ms->file) fwrite(ms->rowSums, rowSumsByteCount, 1, ms->file);
+		if (ms->gtRowSums) addArrayToArray_uint32(ms->gtRowSums, ms->rowSums, ms->height);
+	}
+	if (ms->colSums) {
+		if (ms->colBGs) subtractArrayFromArray_uint32(ms->gtColSums, ms->colBGs, ms->width);
+		if (ms->file) fwrite(ms->colSums, colSumsByteCount, 1, ms->file);
+		if (ms->gtColSums) addArrayToArray_uint32(ms->gtColSums, ms->colSums, ms->width);
+	}
+	
+	ms->imgCount += 1;
+	Py_RETURN_NONE;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+static PyObject* imgsum_setBG(PyObject* self, PyObject* args) {
+	modulestate* ms = PyModule_GetState(self);
+	size_t rowSumsByteCount = ms->height * sizeof(*ms->rowSums);		
+	size_t colSumsByteCount = ms->width * sizeof(*ms->colSums);
+	if (ms->gtRowSums) {
+		if (ms->rowBGs == NULL) ms->rowBGs = malloc(rowSumsByteCount);
+		for (int r=0; r < ms->height; r++) {
+			ms->rowBGs[r] = ms->gtRowSums[r] / ms->imgCount;
+		}
+	}
+	if (ms->gtColSums) {
+		if (ms->colBGs == NULL) ms->colBGs = malloc(colSumsByteCount);
+		for (int c=0; c < ms->width; c++) {
+			ms->colBGs[c] = ms->gtColSums[c] / ms->imgCount;
+		}
+	}
+
+	Py_RETURN_NONE;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+static PyObject* imgsum_stop(PyObject* self, PyObject* args) {
+	modulestate* ms = PyModule_GetState(self);	
+
+	if (ms->gtRowSums) {
+		size_t gtRowSumsByteCount = ms->height * sizeof(*ms->gtRowSums);		
+		if (ms->file) fwrite(ms->gtRowSums, gtRowSumsByteCount, 1, ms->file);
+		free(ms->gtRowSums);
+		ms->gtRowSums = NULL;
+	}
+	if (ms->gtColSums) {
+		size_t gtColSumsByteCount = ms->width * sizeof(*ms->gtColSums);
+		if (ms->file) fwrite(ms->gtColSums, gtColSumsByteCount, 1, ms->file);
+		free(ms->gtColSums);
+		ms->gtColSums = NULL;
+	}
+	if (ms->file) {
+		fclose(ms->file);
+		ms->file = NULL;
+	}
+	if (ms->rowSums) {
+		free(ms->rowSums);
+		ms->rowSums = NULL;
+	}
+	if (ms->colSums) {
+		free(ms->colSums);
+		ms->colSums = NULL;
+	}
+	
 	Py_RETURN_NONE;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 static PyMethodDef imgsum_methods[] = {
-	{"start", (PyCFunction)imgsum_start, METH_VARARGS, NULL},
-	{"sum",   (PyCFunction)imgsum_sum,   METH_VARARGS, NULL},
-	{"stop",  (PyCFunction)imgsum_stop,  METH_NOARGS,  NULL},
-	{NULL,    NULL,                      0,            NULL}
+	{"start",    (PyCFunction)imgsum_start,   METH_VARARGS, NULL},
+	{"set_file", (PyCFunction)imgsum_setFile, METH_VARARGS, NULL},
+	{"sum",      (PyCFunction)imgsum_sum,     METH_VARARGS, NULL},
+	{"set_bg",   (PyCFunction)imgsum_setBG,   METH_NOARGS,  NULL},
+	{"stop",     (PyCFunction)imgsum_stop,    METH_NOARGS,  NULL},
+	{NULL,       NULL,                        0,            NULL}
 };
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from distutils.core import setup, Extension
 
-setup(name='imgsum', version='1.0', \
-      ext_modules=[Extension('imgsum',['imgsum.c'])])
+setup(
+	name='imgsum', 
+	version='1.1',
+	ext_modules=[
+		Extension(
+			'imgsum',
+			['imgsum.c'],
+			extra_compile_args=["-std=c11"],
+		)
+	]
+)
 
